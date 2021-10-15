81728db27bf55610ea426f36421bebc886f841ab
Aryan Jawale <87902607+Aryanjawale@users.noreply.github.com>
2021-10-15 13:34:06 +0530

    Update alive.py

--- SpideyRobot/modules/alive.py
+++ SpideyRobot/modules/alive.py
@@ -3,18 +3,18 @@ import re, os
 from SpideyRobot.events import register
 from SpideyRobot import telethn as tbot
 from SpideyRobot import telethn as tgbot
-PHOTO = "https://telegra.ph/file/1cd6d62ef6e8843e6b1cb.jpg"
+PHOTO = "https://telegra.ph/file/f65cb56e8a7f3f184962b.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
-PIKACHU = "**𝙐𝙢𝙢? 𝙎𝘾𝙊𝙍𝘽𝙐𝙉𝙉𝙔 𝙄𝙎 𝙃𝙀𝙍𝙀💤** \n\n"
   PIKACHU = "**◐ I Aᴍ Aᴅᴠᴀɴᴄᴇᴅ Scorbunny !** \n\n"
   PIKACHU += "**◐ I'ᴍ Wᴏʀᴋɪɴɢ Pʀᴏᴘᴇʀʟʏ**\n\n"
   PIKACHU += "**◐ Scorbunny! : 2.0 Lᴀᴛᴇsᴛ**\n\n"
   PIKACHU += "[**◐ Mʏ Mᴀsᴛᴇʀ :**](t.me/Aryanjawale)\n\n"
   PIKACHU += "**◐ Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ : 1.23.0**\n\n"
-  BUTTON = [[Button.url("Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ", "https://t.me/trainer_zone"), Button.url("LOGS Cʜᴀɴɴᴇʟ", "https://t.me/Scorbunny_logs"),
-BUTTON = [[Button.url("Pigasus Support", "https://t.me/PigasusSupport")]]
+  BUTTON = [[Button.url("Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ", "https://t.me/trainer_zone"),
+  BUTTON = [[Button.url("LOGS Cʜᴀɴɴᴇʟ", "https://t.me/Scorbunny_logs"),
+  BUTTON = [[Button.url("Pigasus Support", "https://t.me/PigasusSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
 
 
